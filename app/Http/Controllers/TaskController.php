48b80a2819ead91c78d18f0dda68859e59cc7808
龔育晴 <ching85117@gmail.com>
2017-12-02 16:50:56 +0800

    在 TaskController 控制器的建構子中對它使用「型別提示」，並在 index 路由中使用它

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,24 +1,30 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Repositories\TaskRepository;
 class TaskController extends Controller
 {
     /**
-     * 建立一個新的控制器實例。
+     * 任務資源庫的實例。
      *
+     * @var TaskRepository
+     */
+    protected $tasks;
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
      * @return void
      */
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+        $this->tasks = $tasks;
     }
-
-     /**
+    /**
      * 顯示使用者所有任務的清單。
      *
      * @param  Request  $request
@@ -27,12 +33,10 @@ class TaskController extends Controller
     public function index(Request $request)
     {
         $tasks = Task::where('user_id', $request->user()->id)->get();
-
         return view('tasks.index', [
-            'tasks' => $tasks,
+            'tasks' => $this->tasks->forUser($request->user()),
         ]);
     }
-
     /**
      * 建立新的任務。
      *
@@ -44,14 +48,10 @@ class TaskController extends Controller
         $this->validate($request, [
             'name' => 'required|max:255',
         ]);
-
         $request->user()->tasks()->create([
             'name' => $request->name,
         ]);
-
         return redirect('/tasks');
-    }
-
-    // Create The Task...
+        // Create The Task...
     }
 }
