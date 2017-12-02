19b3f522cfa7b7b0d3e8814a9b96f54030076430
龔育晴 <ching85117@gmail.com>
2017-12-02 16:40:36 +0800

    建立新的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,14 +18,29 @@ class TaskController extends Controller
         $this->middleware('auth');
     }
 
-+    /**
-+     * 顯示使用者所有任務的清單。
-+     *
-+     * @param  Request  $request
-+     * @return Response
-+     */
-+    public function index(Request $request)
- +    {
- +        return view('tasks.index');
- +    }
+     /**
+     * 顯示使用者所有任務的清單。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+
+    /**
+     * 建立新的任務。
+    *
+    * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+       $this->validate($request, [
+          'name' => 'required|max:255',
+    ]);
+
+    // Create The Task...
+    }
 }
