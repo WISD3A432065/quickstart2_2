675bdeb3b37444c18052231f90127506d6befcbe
龔育晴 <ching85117@gmail.com>
2017-12-02 16:57:02 +0800

    路由模型綁定

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -54,4 +54,15 @@ class TaskController extends Controller
         return redirect('/tasks');
         // Create The Task...
     }
+    /**
+     * 移除給定的任務。
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        //
+    }
 }
